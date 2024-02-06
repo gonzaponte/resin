8ea2eb47ddf985866443873b2d34bc898860e15f
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2024-02-06 12:22:12 +0100

    Write output in parquet format

--- rust/src/bin/analytical.rs
+++ rust/src/bin/analytical.rs
@@ -1,118 +1,129 @@
-use std::io;
-use std::io::{Write, BufWriter};
 use std::fs::File;
 use std::path::PathBuf;
 
-
 use clap::Parser;
-use rand::random;
+use polars::prelude::*;
 use indicatif::ProgressBar;
 use itertools::Itertools;
+use rand::random;
 
 /// CLI
 #[derive(clap::Parser, Debug, Clone)]
 #[clap( name  = "analytical"
       , about = "Create data based on an analytical")]
 pub struct Cli {
-    #[clap(short = 'o', long, default_value = "out.csv")]
+    #[clap(short = 'o', long, default_value = "out.parquet")]
     pub outfile : PathBuf,
+
+    #[clap(short = 'f', long, default_value = "1000")]
+    pub evt_per_file : u64,
 
     #[clap(short = 'j', long, default_value = "4")]
     pub threads : u8,
 
     #[clap(short = 'b', long, default_value = "10")]
-    pub base : u8,
+    pub base : u64,
 
     #[clap(short = 'e', long)]
-    pub exponent : u8,
+    pub exponent : u32,
 }
 
-struct Event {
-    x: f32,
-    y: f32,
-    z: f32,
-    response : Vec<f32>,
+fn new_filename(filename : PathBuf, index : u64) -> PathBuf {
+    let filename    = filename.into_os_string().into_string().unwrap();
+    let split_index = filename.rfind(".").expect("Pattern not found");
+    let (basename, extension) =  filename.split_at(split_index);
+    let new_file = PathBuf::from(format!("{basename}_{index}{extension}"));
+    new_file
 }
 
-struct Pos(f32, f32, f32);
-
-fn generate_random_position() -> Pos {
-    let r  = 200f32;
+fn generate_random_position() -> (f64, f64, f64) {
+    let r  = 200f64;
     let r2 = r*r;
     loop {
-        let x = r * (random::<f32>() - 0.5);
-        let y = r * (random::<f32>() - 0.5);
+        let x = r * (random::<f64>() - 0.5);
+        let y = r * (random::<f64>() - 0.5);
         if x*x + y*y < r2 {
-            let z = 500f32 * random::<f32>();
-            return Pos(x, y, z);
+            let z = 500f64 * random::<f64>();
+            return (x, y, z);
         }
     }
 }
 
-fn psf(x1 : f32, y1 : f32, x0 : f32, y0 : f32) -> f32 {
+fn psf(x1 : f64, y1 : f64, x0 : f64, y0 : f64) -> f64 {
     let dx = x1 - x0;
     let dy = y1 - y0;
-    let dz = 5f32;
+    let dz = 5f64;
     dz.powf(1.5) / (dx*dx + dy*dy + dz*dz).powf(1.5)
 }
 
-fn apply_psf(pos : Pos, sipm_pos : &Vec<(f32, f32)>) -> Event {
-    let Pos(x, y, z) = pos;
-    let response : Vec<f32> =
+fn apply_psf(pos : (f64, f64, f64), sipm_pos : &Vec<(f64, f64)>) -> (f64, f64, f64, Vec<f64>) {
+    let (x, y, z) = pos;
+    let response : Vec<f64> =
     sipm_pos.iter()
             .map(|(xs, ys)| { psf(*xs, *ys, x, y) })
             .collect();
 
-    Event{x, y, z, response}
+    (x, y, z, response)
 }
 
-struct CsvWriter {
-    writer   : BufWriter<File>,
+fn create_df(data : (f64, f64, f64, Vec<f64>)) -> LazyFrame {
+    let response = data.3
+        .into_iter()
+        .enumerate()
+        .map(|(i,v)| { (format!("sipm_{}", i).to_string(),  v) } )
+        .map(|(name, value)| { Series::new(&name, &[value]) } )
+        ;
+
+    let mut columns = vec![
+        Series::new("x", &[data.0]),
+        Series::new("y", &[data.1]),
+        Series::new("z", &[data.2]),
+    ];
+    columns.extend(response);
+    DataFrame::new(columns).unwrap().lazy()
 }
 
-impl CsvWriter {
-    pub fn new(filename : PathBuf) -> Self {
-        let file   = File::create(filename).unwrap();
-        let writer = BufWriter::new(file);
-        Self{writer}
-    }
-
-    pub fn write(&mut self, evt : &Event) -> io::Result<()> {
-        let response = evt.response.iter()
-            .map(f32::to_string)
-            .collect::<Vec<String>>()
-            .join(", ");
-        let line = format!("{} {} {} {}\n", evt.x, evt.y, evt.z, response);
-        self.writer.write(&line.into_bytes())?;
-        Ok(())
-    }
-}
-
-fn sipm_positions() -> Vec<(f32, f32)> {
+fn sipm_positions() -> Vec<(f64, f64)> {
     (0..45).cartesian_product(0..45)
            .map(|(i, j)| {(-220 + i*10, -200 + j*10)})
-           .map(|(x, y)| {(   x as f32,    y as f32)})
+           .map(|(x, y)| {(   x as f64,    y as f64)})
            .collect()
 }
 
 
-fn main() -> io::Result<()> {
+
+fn main() -> Result<(), String> {
     let args = Cli::parse();
     let filename = PathBuf::from(&args.outfile);
-    std::fs::create_dir_all(filename.parent().unwrap())
-        .unwrap_or_else(|e| panic!("Cannot write to destination {}: {e}", args.outfile.display()));
+    std::fs::create_dir_all(filename.parent().expect("Could not access parent directory"))
+        .expect("Cannot write to destination");
 
-    let mut writer = CsvWriter::new(filename);
-    let sipm_pos   = sipm_positions();
+    new_filename(filename.clone(), 123);
 
-    let n    = (args.base as u32).pow(args.exponent as u32) as u64;
-    let pmod = n / 200;
-    let pb   = ProgressBar::new(n);
-    (0..n).into_iter()
-          .map     (|i  | { if i % pmod == 0 {pb.inc(pmod);} })
-          .map     (|_  | { generate_random_position()           })
-          .map     (|pos| { apply_psf(pos, &sipm_pos)       })
-          .for_each(|evt| { writer.write(&evt).expect("") });
+    let sipm_pos = sipm_positions();
 
+    let ntot   = args.base.pow(args.exponent);
+    let nfile  = args.evt_per_file.min(ntot);
+    let nbatch = (ntot as f64 / nfile as f64).round() as u64;
+    assert_eq!(nbatch * nfile, ntot, "Invalid ratio of evt_per_file and ntot");
+
+    let pb     = ProgressBar::new(nbatch);
+    (0..nbatch).into_iter()
+        .inspect(|_| { pb.inc(1);} )
+        .map    (|i| new_filename(args.outfile.clone(), i))
+        .for_each(|filename| {
+            let dfs : Vec<LazyFrame> =
+            (0..nfile).into_iter()
+                       .map     (|_  | { generate_random_position()       })
+                       .map     (|pos| { apply_psf(pos, &sipm_pos)        })
+                       .map     (create_df)
+                       .collect();
+
+            let mut df = concat(&dfs, UnionArgs::default()).unwrap().collect().unwrap();
+
+            let mut file = File::create(filename).unwrap();
+            ParquetWriter::new(&mut file).finish(&mut df).unwrap();
+        });
     Ok(())
+
 }
